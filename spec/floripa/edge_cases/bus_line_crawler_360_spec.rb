bada5bf006a41bec3d1fe847757db13673f35c54
Paulo Ragonha <paulo@ragonha.me>
2014-04-27 14:41:44 -0300

    renames edge case spec

--- spec/floripa/edge_cases/bus_line_crawler_360_spec.rb
+++ spec/floripa/edge_cases/bus_line_crawler_360_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe "bus_crawler" do
-  describe "when fetching the data of a bus" do
+  describe "when fetching the data of the 360 line" do
     subject { FloripaPublicTransit::BusCrawler.new('360').fetch }
 
     it "should work (was breaking because of encoding issues)" do
